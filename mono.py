cc18035dbf3e71bc710b17cace7df8b23faf0552
william <wwwy3y3@gmail.com>
2013-10-13 22:45:25 +0800

    fix orderDict, unique in all alpha

--- mono.py
+++ mono.py
@@ -1,23 +1,31 @@
 from string import ascii_lowercase
-from collections import deque
+from collections import deque, OrderedDict
 
 # read plaintext and key form file
 f= open('text', 'r')
 plaintext= f.readline().lower()
-key= ''.join(set(f.readline())) # unique the string
+key= ''.join(OrderedDict.fromkeys(f.readline()).keys()) # unique the string
+
 
 # init a fillList, from a-z
 #fillList= map(chr, range(97, 123))
 
 # init a table dict, keys from a-z
-table= dict((el,0) for el in ascii_lowercase)
+table= OrderedDict((el,0) for el in ascii_lowercase)
 
 # concat key and alphabets
 fillList= deque()
 for char in key:
 	fillList.append(char)
 
+# delete chars from list
+chars= []
 for char in ascii_lowercase:
+	if char not in key: # if char not appear in key
+		chars.append(char)
+
+# fill the chars
+for char in chars:
 	fillList.append(char)
 
 # fill the table
@@ -27,8 +35,10 @@ for x in table:
 # encrypt the plaintext
 # if char is alpha, then map the table, print it
 # else just print it
+pr= ''
 for char in plaintext:
 	if char.isalpha():
-		print table[char],
+		pr+=table[char]
 	else:
-		print char,
+		pr+=char
+print pr
