fde2a6cca1342c2571f41a3d1f3dfa6448512584
william <wwwy3y3@gmail.com>
2014-05-17 14:08:52 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='classical_ciphers',
+    version='0.1.0',
+    author='wwwy3y3',
+    author_email='wwwy3y3@gmail.com',
+    packages=['classical_ciphers'],
+    url='https://github.com/wwwy3y3/ciphers',
+    license='LICENSE.txt',
+    description='Useful tools for classical ciphers',
+    long_description=open('README.txt').read()
+)
