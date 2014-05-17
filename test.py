5929c85e69e1d19a773e839fd3d699dfeb9bf28a
william <wwwy3y3@gmail.com>
2014-05-17 14:00:27 +0800

    arrange again

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-import decrypt as decrypt
-import encrypt as encrypt
-
-# read file
-path= './plaintext/plaintext4_c.txt'
-fr = open(path, 'r')
-ciphertext= ''.join(c for c in fr.read() if c.isalpha())
-fr.close()
-#print ciphertext
-#write file
-fw = open('test2.txt', 'w+')
+import classical_ciphers as cc
 
 # prime then trans
-decrypter= decrypt.Decrypter('fsferxoupqxdilsmzbvj')
-encrypter= encrypt.Encrypter('abcdef123')
+decrypter= cc.Decrypter('fsferxoupqxdilsmzbvj')
+encrypter= cc.Encrypter('abcdef123')
 #decrypter.keyword('zero').trans(67)
 decrypter.poly('snakekesn')
 encrypter.trans(3).poly('snakekesn')
